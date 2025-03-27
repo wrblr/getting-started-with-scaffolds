634f4bc3ab6f1bb948a55dca719f2f53c246613e
wrblr <wrbell@uchicago.edu>
2025-03-27 00:58:18 +0000

    RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
